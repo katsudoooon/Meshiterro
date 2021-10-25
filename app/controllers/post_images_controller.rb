4ab6736aa9c292dbe3a216a9109299a0a7408c78
katsudoooon <sub62.mail58address@gmail.com>
2021-10-25 15:57:16 +0000

    [Finish] 4-4

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -9,19 +9,26 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     @post_image.save
-    redirect_to post_image_path
+    redirect_to post_images_path
 
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
 
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    @post_comment = PostComment.new
+
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
+
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ccf76721be4bc81455716b5de9efb8cc644f85b820dc8cf42b98f1f4230d202e6d7cc9e95dfc5c0ee127cfdb9ed90ea6cbf633b39172377f3995ed706004fe16'
